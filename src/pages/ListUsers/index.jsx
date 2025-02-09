5ae85d8dcab183f1c6a886cf2c39bdd44f7e82b5
fernandoJesuss <fernando_ffernando@hotmail.com>
2025-02-09 19:10:42 -0300

    Enviando projeto para deploy

---
+++ src/pages/ListUsers/index.jsx
@@ -0,0 +1,71 @@
+import { useEffect, useState } from "react";
+import { useNavigate } from "react-router-dom";
+
+import api from "../../services/api";
+
+import Button from "../../components/Button";
+import TopBackground from "../../components/TopBackground";
+import Trash from "../../assets/trash.svg";
+import {
+  AvatarUser,
+  CardUsers,
+  Container,
+  ContainerUsers,
+  Title,
+  TrashIcon,
+} from "./styles";
+
+function ListUsers() {
+  const [users, setUsers] = useState([]);
+  const navigate = useNavigate();
+
+  useEffect(() => {
+    async function getUsers() {
+      const { data } = await api.get("/usuarios");
+
+      setUsers(data);
+    }
+
+    getUsers();
+  }, []);
+
+  async function deleteUsers(id) {
+    await api.delete(`/usuarios/${id}`);
+
+    const updatedUsers = users.filter((user) => user.id !== id);
+
+    setUsers(updatedUsers);
+  }
+
+  return (
+    <Container>
+      <TopBackground />
+      <Title>Listagem de Usuários</Title>
+      <ContainerUsers>
+        {users.map((user) => (
+          <CardUsers key={user.id}>
+            <AvatarUser
+              src={`https://avatar.iran.liara.run/public?username=${user.id}`}
+            />
+            <div>
+              <h3> Nome: {user.name}</h3>
+              <p> Idade: {user.age}</p>
+              <p> E-mail: {user.email}</p>
+            </div>
+            <TrashIcon
+              src={Trash}
+              alt="icone-lixo"
+              onClick={() => deleteUsers(user.id)}
+            />
+          </CardUsers>
+        ))}
+      </ContainerUsers>
+
+      <Button type="button" onClick={() => navigate("/")}>
+        Voltar
+      </Button>
+    </Container>
+  );
+}
+
+export default ListUsers;
