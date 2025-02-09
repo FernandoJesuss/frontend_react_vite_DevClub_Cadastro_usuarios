765c22b540f7f423010cdb14c7ee09dc9c88edfe
fernandoJesuss <fernando_ffernando@hotmail.com>
2025-02-09 20:00:37 -0300

    config deploy

--- vite.config.js
+++ vite.config.js
@@ -4,5 +4,5 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: "/https://github.com/FernandoJesuss/frontend_react_vite_DevClub_Cadastro_usuarios",
+  base: "/frontend_react_vite_DevClub_Cadastro_usuario",
 })
